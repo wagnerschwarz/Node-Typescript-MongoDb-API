8383e4fd728e8d8f770649bb49e06c8fcb8df34c
Wagner Schwarz <wagnerschwarz@outlook.com>
2023-05-27 14:16:16 -0300

    feat: add GetUsersController

---
+++ src/controllers/get-users/get-users.ts
@@ -0,0 +1,23 @@
+import { IGetUsersController, iGetUsersRepository } from "./protocols";
+
+export class GetUsersController implements IGetUsersController {
+  constructor(private readonly getUserRepository: iGetUsersRepository) {}
+
+  async handle() {
+    try {
+      //validar requisição
+      // direcionar chamada para o repository
+      const users = await this.getUserRepository.getUsers();
+
+      return {
+        statusCode: 200,
+        body: users,
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: "Something went wrong.",
+      };
+    }
+  }
+}
