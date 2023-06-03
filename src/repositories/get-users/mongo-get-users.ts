678dce3a83b5956f88d10502846a1d651ad7beb8
Wagner Schwarz <wagnerschwarz@outlook.com>
2023-06-03 08:44:04 -0300

    feat: add id to User

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,8 +1,16 @@
 import { iGetUsersRepository } from "../../controllers/get-users/protocols";
+import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/user";
 
 export class MongoGetUsersRepository implements iGetUsersRepository {
   async getUsers(): Promise<User[]> {
+    const users = await MongoClient.db
+      .collection<User>("users")
+      .find({})
+      .toArray();
+
+    users[0]._id;
+
     return [
       {
         firstName: "Wagner",
